b217cde68be65495477a55483d6d80f136142f3c
Cubxity <contact@cubxity.dev>
2021-04-24 10:01:27 +0200

    chore: bump velocity to 1.1.5

--- velocity/update.sh
+++ velocity/update.sh
@@ -4,7 +4,7 @@ runtimes[adopt8]="adoptopenjdk/openjdk8:jre8u275-b01-alpine"
 runtimes[adopt11]="adoptopenjdk/openjdk11:jre-11.0.9.1_1-alpine"
 runtimes[adopt15]="adoptopenjdk/openjdk15:jre-15.0.1_9-alpine"
 
-version="1.1.4"
+version="1.1.5"
 
 for runtimeName in "${!runtimes[@]}"; do
   runtime="${runtimes[$runtimeName]}"
