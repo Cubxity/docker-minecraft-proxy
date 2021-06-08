412c78dbbae04193a350574d3365de60a36a2bf2
Cubxity <contact@cubxity.dev>
2021-06-08 15:58:51 +0200

    feat: upgrade to Velocity 1.1.7

--- velocity/update.sh
+++ velocity/update.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 declare -A runtimes
-runtimes[adopt8]="adoptopenjdk/openjdk8:jre8u275-b01-alpine"
-runtimes[adopt11]="adoptopenjdk/openjdk11:jre-11.0.9.1_1-alpine"
 runtimes[adopt16]="adoptopenjdk/openjdk16:jre-16.0.1_9-alpine"
 
-version="1.1.5"
+version="1.1.7"
 
 for runtimeName in "${!runtimes[@]}"; do
   runtime="${runtimes[$runtimeName]}"
