b1d4e26fd631abdb75fd574c3a9e85b9661d18a1
Simon <67807644+KoxSosen@users.noreply.github.com>
2022-01-02 09:15:34 +0100

    feat: update velocity to paper api
    * Paper API
    
    * Update readme example with changes
    
    * cleanup
    
    * Version -> Build
    
    * Add / to Alpine
    
    * And Debian as well. :P

--- velocity/build.sh
+++ velocity/build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 version=$(curl -s "https://nexus.velocitypowered.com/repository/maven-public/com/velocitypowered/velocity-native/maven-metadata.xml" | sed -n 's/.*<release>\([^<]*\)<\/release>/\1/p')
 
-echo "[velocity] building velocity-$version-$RUNTIME_NAME"
+curl -s https://papermc.io/api/v2/projects/velocity/ | jq -r -c ".versions[]" | while read -r version; do
+  build=$(curl -s "https://papermc.io/api/v2/projects/velocity/versions/$version" | jq -r ".builds[-1]")
+  hash=$(curl -s "https://papermc.io/api/v2/projects/velocity/versions/$version/builds/$build" | jq -r ".downloads.application.sha256")
+
+echo "[velocity] building velocity-$version-$build-$RUNTIME_NAME"
 
 docker buildx build \
   --build-arg "RUNTIME=$RUNTIME_IMAGE" \
   --build-arg "RUNTIME_NAME=$RUNTIME_NAME" \
   --build-arg "VELOCITY_VERSION=$version" \
+  --build-arg "VELOCITY_BUILD=$build" \
+  --build-arg "VELOCITY_SHA256=$hash" \
   --tag "$REPOSITORY:velocity-$version-$RUNTIME_NAME" \
   --tag "$REPOSITORY:velocity-$RUNTIME_NAME" \
   --cache-from "type=registry,ref=$REPOSITORY:velocity-$version-$RUNTIME_NAME" \
@@ -15,3 +21,4 @@ docker buildx build \
   --platform "$RUNTIME_PLATFORM" \
   --push \
   . || exit 1
+done
